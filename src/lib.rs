1bb794dec84890af0fa87b6db069158041f78f9c
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-04 12:26:24 -0500

    Started grammar

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod parsing;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ src/parsing/parser.rs
@@ -0,0 +1,10 @@
+use pest::iterators::Pair;
+use std::fs;
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"] // relative to src
+struct PestCortexParser;
+
+
