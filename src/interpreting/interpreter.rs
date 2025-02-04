a402708ef5eae80b1c96707f62e5c1fe680af73a
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-04 18:08:49 -0500

    Finished skeleton of evaluate expression funcs

--- src/interpreting/interpreter.rs
+++ src/interpreting/interpreter.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use crate::parsing::ast::expression::{Atom, Expression, PathIdent};
+use crate::parsing::ast::expression::{Atom, Expression, ExpressionTail, PathIdent};
 use super::value::CortexValue;
 
 pub struct CortexInterpreter {
@@ -8,8 +8,9 @@ pub struct CortexInterpreter {
 
 impl CortexInterpreter {
     pub fn evaluate_expression(&self, expr: &Expression) -> Result<CortexValue, Box<dyn Error>> {
-
-        Ok(CortexValue::Void)
+        let atom_result = self.evaluate_atom(&expr.atom)?;
+        let tail_result = self.handle_expr_tail(atom_result, &expr.tail)?;
+        Ok(tail_result)
     }
 
     fn evaluate_atom(&self, atom: &Atom) -> Result<CortexValue, Box<dyn Error>> {
@@ -22,6 +23,11 @@ impl CortexInterpreter {
             Atom::Expression(expr) => Ok(self.evaluate_expression(expr)?),
             Atom::PathIdent(path) => Ok(self.lookup_value(path)?),
             Atom::Call(path_ident, expressions) => todo!(),
+        }
+    }
+    fn handle_expr_tail(&self, atom: CortexValue, tail: &ExpressionTail) -> Result<CortexValue, Box<dyn Error>> {
+        match tail {
+            ExpressionTail::None => Ok(atom),
         }
     }
 
