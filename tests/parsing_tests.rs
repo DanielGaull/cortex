dcf467b228f5fbbd568dc9a6934c1c133422ea8f
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-25 13:07:54 -0500

    Added some additional parsing tests

--- tests/parsing_tests.rs
+++ tests/parsing_tests.rs
@@ -99,6 +99,8 @@ fn test_functions() -> Result<(), Box<dyn Error>> {
     run_function_test("fn test(x: number): void {\n    const x: number = 5;\n    x;\n}")?;
     run_function_test("fn test(x: number): number {\n    const x: number = 5;\n    x\n}")?;
     run_function_test("fn ~(x: number): void {\n    stop;\n}")?;
+    run_function_test("fn test(): void {\n    stop;\n}")?;
+    run_function_test("fn ~(): void {\n    stop;\n}")?;
     Ok(())
 }
 
